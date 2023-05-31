76fb5b07338e07fa6a818fb9ff8c7dd44603f57d
kwamebrowne <theeecryptoking@gmail.com>
2023-05-31 16:15:34 -0500

    fourth attempt

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -1,5 +1,6 @@
 class NationalPark:
 
+
     def __init__(self, name):
         self.name = name
         self._trips = []
@@ -21,16 +22,17 @@ class NationalPark:
     def trips(self, new_trip=None):
         from classes.trip import Trip
         # also forgot to add new_trip to check it wasincluded beofre i add to list 
-        if new_trip and isinstance(new_trip, Trip) :
+        if new_trip and isinstance(new_trip, Trip):
             self._trips.append(new_trip)
         return self._trips
         pass
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        if new_visitor not in self._visitors and isinstance(new_visitor, Visitor):
+        if isinstance(new_visitor, Visitor):
+            # new_visitor not in self._visitors and takend out 
             self._visitors.append(new_visitor)
-        return self._visitors
+        return set(self._visitors)
         pass
     
     def total_visits(self):
@@ -38,15 +40,16 @@ class NationalPark:
         pass
     
     def best_visitor(self):
-        # return max(set(self._visitors), key=self._visitors.count)
-        visitor = None
-        visitamnt = 0 
-        for v in self._visitors:
-            visits = self._visitors.count(v)
-            if visits > visitamnt:
-                visitor = v
-                visitamnt = visits
-        return visitor
-
+        return max(set(self._visitors), key=self._visitors.count)
+        # visitor = None
+        # visitamnt = 0 
+        # for v in self._visitors:
+        #     visits = self._visitors.count(v)
+        #     if visits > visitamnt:
+        #         visitor = v
+        #         visitamnt = visits
+        # return visitor
     # rabs main from the 
         pass
+
+dandelion = NationalPark("becca")
