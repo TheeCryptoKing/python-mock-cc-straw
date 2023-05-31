61c481158d0ec5a4cbeddefb0c362d5c2433a733
kwamebrowne <theeecryptoking@gmail.com>
2023-05-31 11:28:13 -0500

    finished 30 mins ago

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,17 +4,49 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        # test messes this up and both of them require this conditional
+        if not hasattr(self, 'name') and isinstance(name, str) and 1<= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception 
+    
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
+        # also forgot to add new_trip to check it wasincluded beofre i add to list 
+        if new_trip and isinstance(new_trip, Trip) :
+            self._trips.append(new_trip)
+        return self._trips
         pass
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
+        if new_visitor not in self._visitors and isinstance(new_visitor, Visitor):
+            self._visitors.append(new_visitor)
+        return self._visitors
         pass
     
     def total_visits(self):
+        return len(self._trips)
         pass
     
     def best_visitor(self):
+        # return max(set(self._visitors), key=self._visitors.count)
+        visitor = None
+        visitamnt = 0 
+        for v in self._visitors:
+            visits = self._visitors.count(v)
+            if visits > visitamnt:
+                visitor = v
+                visitamnt = visits
+        return visitor
+
+    # rabs main from the 
         pass
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -2,11 +2,33 @@ class Visitor:
 
     def __init__(self, name):
         self.name = name
+        self._trips = []
+        self._parkList = []
         
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        # test messes this up and both of them require this conditional
+        if isinstance(name, str) and not hasattr(self, 'name')  and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception('Wtf dawg')
+    
+    
     def trips(self, new_trip=None):
         from classes.trip import Trip
+        if new_trip and isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
         pass
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
+        if new_national_park not in self._parkList and isinstance(new_national_park, NationalPark):
+            self._parkList.append(new_national_park)
+        return self._parkList
         pass
+
